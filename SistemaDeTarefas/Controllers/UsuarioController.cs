42ee5c67805df91ed51861de261c1e341e307d45
Kenzo Mateus <kenzomateus44@gmail.com>
2023-10-01 21:24:21 -0300

    Create Controller, Models, Data and Repository

---
+++ SistemaDeTarefas/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        // Buscar usuarios
+        [HttpGet]
+        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios() // endpoint
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SistemaDeTarefas/Data/SistemaTarefasDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemaTarefasDBContext : DbContext
+    {
+        public SistemaTarefasDBContext(DbContextOptions<SistemaTarefasDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; } // representa uma tabela do banco de dados
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
